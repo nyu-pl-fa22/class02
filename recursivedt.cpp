c54fc9191e1669eb0490ee9b79fbac69e6e346e7
Thomas Wies <wies@cs.nyu.edu>
2022-09-13 22:36:19 -0400

    minor changes in code

--- recursivedt.cpp
+++ recursivedt.cpp
@@ -1,11 +1,29 @@
-struct manager;              // Declaration
-struct employee {
-  struct manager* boss;
-  struct employee* next_employee;
-  ...
+struct Manager;              // Declaration
+struct Employee {
+  Manager* boss;
+  Employee* next_employee;
 };
 
-struct manager {             // Definition
-  struct employee* first_employee;
-  ...
+struct Manager {             // Definition
+  Employee* first_employee;
 };
+
+
+Manager* create() {
+  Employee* e = new Employee();
+  Manager* m = new Manager();
+
+  e->boss = m;
+  m->first_employee = e;
+
+  return m;
+}
+
+int main() {
+  Manager* m = create();
+
+  delete(m->first_employee);
+  delete(m);
+  
+  return 0;
+}
